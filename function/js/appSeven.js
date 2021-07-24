a09ba3c0ca264cfe2eabfdae382ab58e6f832d35
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-07-24 19:44:52 +0700

    add function folder

---
+++ function/js/appSeven.js
@@ -0,0 +1,8 @@
+// Define function
+function Subtraction(x, y) {
+	return x - y;
+}
+
+// Invoke function
+document.write("Subtraction of 10 and 5 = ", Subtraction(10, 5), "<br>");
+document.write("Subtraction of 20 and 10 = ", Subtraction(20, 10), "<br>");
---
+++ function/js/appTwo.js
@@ -0,0 +1,15 @@
+// Define function
+function MyFunctionGreeting() {
+	document.write("Hello JavaScript", "<br>");
+}
+function MyFunctionName() {
+	document.write("My Name is Kok", "<br>");
+}
+function MyFunctionGender() {
+	document.write("My Gender is Male", "<br>");
+}
+
+// Invoke function
+MyFunctionGreeting();
+MyFunctionName();
+MyFunctionGender();
