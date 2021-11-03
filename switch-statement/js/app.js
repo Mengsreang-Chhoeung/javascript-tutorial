b9d4257caa98b72bc1746b834b0f6257da6bf4c2
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-11-03 20:24:23 +0700

    add switch-statement example

---
+++ switch-statement/js/app.js
@@ -0,0 +1,29 @@
+// initial value
+var date = 3;
+// check condition with switch statement
+switch (date) {
+  case 0:
+    document.write("Sunday");
+    break;
+  case 1:
+    document.write("Monday");
+    break;
+  case 2:
+    document.write("Tuesday");
+    break;
+  case 3:
+    document.write("Wednesday");
+    break;
+  case 4:
+    document.write("Thursday");
+    break;
+  case 5:
+    document.write("Friday");
+    break;
+  case 6:
+    document.write("Saturday");
+    break;
+  default:
+   	document.write("Invalid day");
+   	break;
+}
---
+++ switch-statement/js/appTwo.js
@@ -0,0 +1,20 @@
+// initial value
+var character = 'a';
+// check condition with switch statement
+switch (character) {
+  case 'A':
+  case 'a':
+    document.write("Character A");
+    break;
+  case 'B':
+  case 'b':
+    document.write("Character B");
+    break;
+  case 'C':
+  case 'c':
+    document.write("Character C");
+    break;
+  default:
+   	document.write("Invalid character");
+   	break;
+}
