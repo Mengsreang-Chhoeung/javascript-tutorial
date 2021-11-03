b39fa3fede700080abef603c7722a0d8646e65a6
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-11-03 20:11:12 +0700

    add condition example

---
+++ condition/js/app.js
@@ -0,0 +1,10 @@
+// initial value
+var age = 10;
+// check condition
+if (age < 18) {
+	document.write("You are Children");
+}
+
+
+
+
---
+++ condition/js/appTwo.js
@@ -0,0 +1,10 @@
+// initial value
+var age = 10;
+// check condition
+if (age < 18) {
+	document.write("You are Children");
+} else {
+	document.write("You are Old");
+}
+
+
