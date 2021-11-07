c2b39c89bdcf0ef8ba3d933410e3f5dcc191875d
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-11-07 12:34:21 +0700

    add break-continue-statement

---
+++ break-continue-statement/js/app.js
@@ -0,0 +1,10 @@
+// using break statement in for loop
+for (var i = 1; i <= 5; i++) {
+  if (i === 4) break; 
+  document.write("Number " + i + "<br>");
+}
+
+
+
+
+
---
+++ break-continue-statement/js/appTwo.js
@@ -0,0 +1,10 @@
+// using continue statement in for loop
+for (var i = 1; i <= 5; i++) {
+  if (i === 4)  continue;
+  document.write("Number " + i + "<br>");
+}
+
+
+
+
+
