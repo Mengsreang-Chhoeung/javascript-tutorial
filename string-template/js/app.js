457e8004bd5a4402afb78c9d9a30bbcbbe10dc02
Mengsreang Chhoeung <mengsreangchhoeung789@gmail.com>
2021-11-15 19:56:49 +0700

    add string-template folder

---
+++ string-template/js/app.js
@@ -0,0 +1,14 @@
+// initial value
+var txtOne = `Hello World!`;
+var txtTwo = `He's often called "Johnny"`; 
+var txtThree = `
+	The quick
+	brown fox
+	jumps over
+	the lazy dog
+`;
+
+// output
+document.write("Text One: " + txtOne + "<br>");
+document.write("Text Two: " + txtTwo + "<br>");
+document.write("Text Three: " + txtThree + "<br>");
---
+++ string-template/js/appTwo.js
@@ -0,0 +1,10 @@
+// initial value
+var firstName = "John";
+var lastName = "Doe";
+
+// output with ${...}
+document.write(`Welcome ${firstName}, ${lastName}!<br><br>`);
+document.write(`Number ${10 + 20}!<br><br>`);
+document.write(`Random Number ${Math.floor(Math.random() * 1000)}!<br><br>`);
+
+
